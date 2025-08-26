69ba677c5af7d5911bd9b08c0c15ad775a51f5c4
Gabrielly Santos Lima <99834050+gabrielly-slima@users.noreply.github.com>
2025-08-25 21:23:02 -0300

    Add files via upload

--- calculando-soma.c
+++ calculando-soma.c
@@ -1,5 +1,5 @@
 /*
-Nome...: Francisco Pereira Junior
+Nome...: Gabrielly Santos Lima
 Turma..: EC41A-C11
 Data...: 25/05/2025
 Enunciado1.: Inicialize 2 variáveis (float) e mostre a soma dos valores na tela.
@@ -8,6 +8,7 @@ Enunciado3.: Idem, mas mostrando na tela além do resultado, os valores envolvid
 no cálculo.
 */
 
+/*
 #include <stdio.h>
 
 int main() {
@@ -18,6 +19,46 @@ int main() {
     result = valor1 + valor2;
     
     printf("%f", result);
+
+    return 0;
+}
+*/
+
+/*
+#include <stdio.h>
+
+int main() {
+    float valor1, valor2, result;
+
+    printf("Digite um valor: ");
+    scanf("%f",& valor1);
+
+    printf("Digite outro valor: ");
+    scanf("%f",& valor2);
+
+    result = valor1 + valor2;
+    
+    printf("%f", result);
+
+    return 0;
+}
+*/
+#include <stdio.h>
+
+int main() {
+    float valor1, valor2, result;
+
+    printf("Digite um valor: ");
+    scanf("%f",& valor1);
+
+    printf("Digite outro valor: ");
+    scanf("%f",& valor2);
+
+    result = valor1 + valor2;
+
+    printf("%f", result);
+    printf("\n%f + %f = %f", valor1, valor2, result);
+    
 
     return 0;
 }
