2cd66727c78b8cd9edaee2a9e12e7af653168f5c
Gabrielly Santos Lima <99834050+gabrielly-slima@users.noreply.github.com>
2025-09-12 17:49:58 -0300

    Update 2-verificacao-de-numero-par.c

--- exercicios-fixacao-020925/2-verificacao-de-numero-par.c
+++ exercicios-fixacao-020925/2-verificacao-de-numero-par.c
@@ -1,1 +1,30 @@
+/*
+Nome...: Gabrielly Santos Lima
+Turma...: EC41A-C11
+Data...: 02/09/2025
+Enunciado.: FIX03_EX07: Faça um programa em linguagem C que peça um número inteiro e
+apresente a mensagem “é par” ou “é ímpar”.
+Dica: use o operador % (resto da divisão de inteiros).
+Por exemplo: 5 % 2 retornará o resto da divisão de 5 por 2.
+Então, 5 % 2 = 1 (porque 5 dividido por 2 é igual a 2 e resta 1).
+Por outro lado, 6 % 2 = 0, porque 6 dividido por 2 é igual a 3 e resta 0
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int num = 0;
+
+    printf("Digite um numero: ");
+    scanf("%d", &num);
+
+    if(num % 2 == 0) {
+        printf("%d é par", num);
+    } else {
+        printf("%d é ímpar", num);
+    }
+
+    return 0;
+}
 
