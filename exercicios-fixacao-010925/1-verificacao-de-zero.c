3aa63c538d9343276bf4f6f41b94d3183934a9e4
Gabrielly Santos Lima <99834050+gabrielly-slima@users.noreply.github.com>
2025-09-12 17:53:18 -0300

    Update 1-verificacao-de-zero.c

--- exercicios-fixacao-010925/1-verificacao-de-zero.c
+++ exercicios-fixacao-010925/1-verificacao-de-zero.c
@@ -1,1 +1,24 @@
+/*
+Nome...: Gabrielly Santos Lima
+Turma...: EC41A-C11
+Data...: 01/09/2025
+Enunciado.: FIX03_EX01: Faça um programa em linguagem C que leia uma valor do
+usuário e diga se ele é um valor igual a zero.
+*/
+
+#include <stdio.h>
+
+int main() {
+    int valor = 0;
+
+    printf("Digite um valor: ");
+    scanf("%d", &valor);
+
+    if(valor == 0) {
+        printf("\nValor igual a zero");
+    }
+
+    printf("\nFinalizando o programa");
+
+}
 
