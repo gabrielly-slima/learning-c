cfd18dd7ec481efc92665f1c7ea4d17ac6c8faf0
Gabrielly Santos Lima <99834050+gabrielly-slima@users.noreply.github.com>
2025-09-12 18:04:09 -0300

    Create 2-meses-do-ano.c

---
+++ exercicios-fixacao-080925/2-meses-do-ano.c
@@ -0,0 +1,8 @@
+/*
+Nome...: Gabrielly Santos Lima
+Turma...: EC41A-C11
+Data...: 08/09/2025
+Enunciado...:FIX03_EX11: Faça um programa em Linguagem C que peça o mês do ano (um número de 1 a 12). 
+O algoritmo deve apresentar o nome do mês por extenso e quantos dias o mês tem, ou “mês inválido”,
+caso digite um número fora do intervalo válido.
+*/
